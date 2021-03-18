b0fa46f77bf7dfc6066e383835ccb9559925526c
Rei <chloerei@gmail.com>
2021-03-18 21:08:22 +0800

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :auth_token
 
+  validates :name, presence: true
   validates :email, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
 
   before_validation :set_space
