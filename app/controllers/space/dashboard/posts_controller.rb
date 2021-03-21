22dfc11e58d138808c9ab34b4d916e7596b5666b
Rei <chloerei@gmail.com>
2021-03-21 19:47:22 +0800

    Add post new view

--- app/controllers/space/dashboard/posts_controller.rb
+++ app/controllers/space/dashboard/posts_controller.rb
@@ -1,4 +1,12 @@
 class Space::Dashboard::PostsController < Space::Dashboard::BaseController
   def index
   end
+
+  def new
+    @post = @space.posts.new
+    render layout: 'base'
+  end
+
+  def create
+  end
 end
