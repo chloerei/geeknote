a2ae83a82c15b82a9374be466171bbb603b7d782
Rei <chloerei@gmail.com>
2021-04-08 21:48:03 +0800

    Rename post summary to excerpt

--- app/controllers/space/dashboard/posts/settings_controller.rb
+++ app/controllers/space/dashboard/posts/settings_controller.rb
@@ -15,6 +15,6 @@ class Space::Dashboard::Posts::SettingsController < Space::Dashboard::Posts::Bas
   private
 
   def settings_params
-    params.require(:post).permit(:summary)
+    params.require(:post).permit(:excerpt)
   end
 end
