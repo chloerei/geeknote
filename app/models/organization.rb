40de548ee1cf1f717990e8a1b376f5724593246f
Rei <chloerei@gmail.com>
2021-05-22 22:16:52 +0800

    change account profile header, add cover

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,7 @@ class Organization < ApplicationRecord
   has_one :account, as: :owner, autosave: true
   has_many :memberships
   has_one_attached :avatar
+  has_one_attached :cover
 
   validates :name, presence: true
 
