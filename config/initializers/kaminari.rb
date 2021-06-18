383d2583e4dead9d1bc875d2c7a82772168c761c
Rei <chloerei@gmail.com>
2021-06-18 17:25:13 +0800

    change kaminari config

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -1,20 +1,12 @@
 Kaminari.configure do |config|
   config.default_per_page = 20
   # config.max_per_page = nil
-  # config.window = 2
-  # config.outer_window = 0
+  config.window = 2
+  config.outer_window = 1
   # config.left = 0
   # config.right = 0
   # config.page_method_name = :page
   # config.param_name = :page
   # config.max_pages = nil
   # config.params_on_first_page = false
-end
-
-class Kaminari::Helpers::Paginator
-  # alway display pagination
-  def render(&block)
-    instance_eval(&block)
-    @output_buffer
-  end
 end
