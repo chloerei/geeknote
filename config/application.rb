e9d2856a4cf5bdcd78879f52e72dc19c7d41a0b8
Rei <chloerei@gmail.com>
2021-07-01 22:15:41 +0800

    Add smtp settings

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,6 @@ module GeekNote
       expires_in: 1.day
     }
 
-    config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
     config.active_job.queue_adapter = :sidekiq
 
@@ -57,5 +56,26 @@ module GeekNote
     end
 
     config.active_storage.service = ENV['STORAGE_SERVICE'] || :local
+
+    if ENV['MAILER_DELIVERY_METHOD'] == 'smtp'
+      config.action_mailer.delivery_method = :smtp
+      config.action_mailer.smtp_settings = {
+        address: ENV['SMTP_ADDRESS'],
+        port: ENV['SMTP_PORT'],
+        domain: ENV['SMTP_DOMAIN'],
+        user_name: ENV['SMTP_USERNAME'],
+        password: ENV['SMTP_PASSWORD'],
+        authentication: 'plain',
+        enable_starttls_auto: true,
+        ssl: ENV['SMTP_SSL'].present?
+      }
+    end
+
+    config.action_mailer.default_options = {
+      from: ENV['MAILER_DEFAULT_FROM']
+    }
+    config.action_mailer.default_url_options = {
+      host: ENV['HOST']
+    }
   end
 end
