dac0e38db30ed060bfba7ecd9e4db1bd87b56f90
Rei <chloerei@gmail.com>
2021-07-01 16:35:23 +0800

    Add account name exclustion list

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,6 +10,29 @@ class Account < ApplicationRecord
   NAME_REGEXP = /\A[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\z/
   validates :name, uniqueness: true, format: { with: NAME_REGEXP }, presence: true
 
+  NAME_EXCLUSION_LIST = %w(
+    admin
+    attachments
+    bookmarks
+    login
+    notifications
+    organizations
+    posts
+    rails
+    sessions
+    settings
+    tags
+    users
+  ) + ENV.fetch('ACCOUNT_NAME_EXCLUSION_LIST', '').split(',')
+
+  validate :validate_name_exclusion
+
+  def validate_name_exclusion
+    if name && name.downcase.in?(NAME_EXCLUSION_LIST)
+      errors.add(:name, :exclusion)
+    end
+  end
+
   def to_param
     name
   end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,4 +1,19 @@
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
+  test "should validate name exclusion" do
+    account = build(:account)
+
+    account.name = 'notspecial'
+    account.valid?
+    assert_not account.errors.added? :name, :exclusion
+
+    account.name = 'admin'
+    account.valid?
+    assert account.errors.added? :name, :exclusion
+
+    account.name = 'Admin'
+    account.valid?
+    assert account.errors.added? :name, :exclusion
+  end
 end
