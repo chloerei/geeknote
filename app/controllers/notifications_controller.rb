903079da5b421ce08d92714f38d0a77afada5dae
Rei <chloerei@gmail.com>
2021-07-03 00:00:56 +0800

    set notification order

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :require_sign_in
 
   def index
-    @notifications = current_user.notifications.page(params[:page])
+    @notifications = current_user.notifications.order(id: :desc).page(params[:page])
     current_user.notifications.unread.update_all(read_at: Time.now)
   end
 end
