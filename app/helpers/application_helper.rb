401afdabc0d253e9c4c94d39b8e88d47beb652e9
Rei <chloerei@gmail.com>
2021-07-06 21:04:17 +0800

    edit post item design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -66,6 +66,16 @@ module ApplicationHelper
     end
   end
 
+  def featured_image_thumb_tag(featured_image)
+    if featured_image.attached?
+      if use_aliyun_oss?
+        image_tag featured_image.url(params: { 'x-oss-process' => 'image/resize,m_fill,h_400,w_260'})
+      else
+        image_tag featured_image.variant(resize_to_fill: [400, 260])
+      end
+    end
+  end
+
   def post_summary(post)
     if post.excerpt.present?
       truncate post.excerpt, length: 100
