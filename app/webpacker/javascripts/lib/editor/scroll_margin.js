691d36a2eb7fb36583cf4638702d826d670c9454
Rei <chloerei@gmail.com>
2021-07-09 22:47:48 +0800

    edit editor layout

--- app/webpacker/javascripts/lib/editor/scroll_margin.js
+++ app/webpacker/javascripts/lib/editor/scroll_margin.js
@@ -3,23 +3,12 @@ import { ViewPlugin, PluginField } from "@codemirror/view"
 export function scrollMargin() {
   return ViewPlugin.fromClass(class {
     margin = {
-      bottom: 0
+      bottom: 120
     }
 
     constructor(view) {
     }
 
-    update(update) {
-      if (update.docChanged) {
-        this.margin = {
-          bottom: 100
-        }
-      } else {
-        this.margin = {
-          bottom: 0
-        }
-      }
-    }
   }, {
     provide: PluginField.scrollMargins.from(value => value.margin)
   })
