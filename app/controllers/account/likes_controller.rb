1c8e4cff48b9d740dd92951802db28784ab4a6a8
Rei <chloerei@gmail.com>
2021-07-14 14:55:09 +0800

    only show published post in likes tab

--- app/controllers/account/likes_controller.rb
+++ app/controllers/account/likes_controller.rb
@@ -2,6 +2,6 @@ class Account::LikesController < Account::BaseController
   before_action :require_user_account
 
   def index
-    @posts = @account.owner.liked_posts.order("likes.created_at": :desc).page(params[:page])
+    @posts = @account.owner.liked_posts.published.order("likes.created_at": :desc).page(params[:page])
   end
 end
