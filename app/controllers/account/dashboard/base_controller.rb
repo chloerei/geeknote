2429654847fe95d2439d4286ce97af352262120d
Rei <chloerei@gmail.com>
2021-07-29 21:47:21 +0800

    change dashboard layout

--- app/controllers/account/dashboard/base_controller.rb
+++ app/controllers/account/dashboard/base_controller.rb
@@ -1,8 +1,6 @@
 class Account::Dashboard::BaseController < Account::BaseController
   before_action :require_sign_in, :require_account_member
 
-  layout 'dashboard'
-
   private
 
   def require_account_member
