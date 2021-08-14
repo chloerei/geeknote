eb2430b18ffe874eae38cf5b336210f5e32ee41d
Rei <chloerei@gmail.com>
2021-08-14 21:57:02 +0800

    Add sortable and draggable js controller

---
+++ app/webpacker/javascripts/controllers/draggable_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["handle"]
+
+  connect() {
+    if (this.handleTarget) {
+      this.handleTarget.addEventListener('mousedown', () => {
+        this.element.setAttribute('draggable', 'true')
+      })
+
+      this.handleTarget.addEventListener('mouseup', () => {
+        this.element.removeAttribute('draggable')
+      })
+    }
+
+    this.element.addEventListener('dragstart', () => {
+      setTimeout(() => {
+        this.element.classList.add('dragging')
+      }, 1)
+    })
+
+    this.element.addEventListener('dragend', () => {
+      this.element.classList.remove('dragging')
+
+      if (this.handleTarget) {
+        this.element.removeAttribute('draggable')
+      }
+    })
+  }
+}
---
+++ app/webpacker/javascripts/controllers/sortable_controller.js
@@ -0,0 +1,74 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["item"]
+
+  connect() {
+    this.clean()
+
+    this.element.addEventListener('dragstart', this.dragstart.bind(this))
+    this.element.addEventListener('dragend', this.dragend.bind(this))
+
+    this.element.addEventListener('dragover', this.dragover.bind(this))
+    this.element.addEventListener('drop', this.drop.bind(this))
+  }
+
+  clean() {
+    this.draggingItem = null;
+    this.dropItem = null;
+    this.dropPosition = null;
+  }
+
+  findItem(target) {
+    return target.closest('[data-sortable-target="item"]')
+  }
+
+  dragstart(event) {
+    event.dataTransfer.effectAllowed = "move"
+    this.draggingItem = this.findItem(event.target)
+  }
+
+  dragend(event) {
+    this.clean()
+  }
+
+  dragover(event) {
+    event.preventDefault()
+
+    if (this.draggingItem) {
+      event.dataTransfer.dropEffect = "move"
+      let dragoverItem = this.findItem(event.target)
+
+      if (dragoverItem && dragoverItem != this.draggingItem) {
+
+        let rect = dragoverItem.getBoundingClientRect()
+        let center = rect.top + rect.height / 2
+
+        if (event.clientY < center) {
+          if (this.dropPlaceChanged(dragoverItem, 'before')) {
+            dragoverItem.insertAdjacentElement('beforebegin', this.draggingItem)
+            this.setDropPlace(dragoverItem, 'before')
+          }
+        } else {
+          if (this.dropPlaceChanged(dragoverItem, 'after')) {
+            dragoverItem.insertAdjacentElement('afterend', this.draggingItem)
+            this.setDropPlace(dragoverItem, 'after')
+          }
+        }
+      }
+    }
+  }
+
+  dropPlaceChanged(item, position) {
+    return this.dropItem != item || this.dropPosition != position
+  }
+
+  setDropPlace(item, position) {
+    this.dropItem = item
+    this.dropPosition = position
+  }
+
+  drop(event) {
+    event.preventDefault()
+  }
+}
