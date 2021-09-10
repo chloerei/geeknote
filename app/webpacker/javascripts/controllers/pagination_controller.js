fd96ca6bfe12a32ce2a3331c57548520f472f668
Rei <chloerei@gmail.com>
2021-09-10 21:45:09 +0800

    Add pagination controller js

---
+++ app/webpacker/javascripts/controllers/pagination_controller.js
@@ -0,0 +1,50 @@
+import { Controller } from "stimulus"
+import Rails from "@rails/ujs"
+
+export default class extends Controller {
+  static targets = ["nextPageLink"]
+
+  static values = {
+    rootMargin: String
+  }
+
+  connect() {
+    this.observeNextPageLink()
+  }
+
+  observeNextPageLink() {
+    if (this.hasNextPageLinkTarget) {
+      let observer = new IntersectionObserver((entries, observer) => {
+        if (entries[0].isIntersecting) {
+          observer.disconnect()
+          this.loadNextPage()
+        }
+      }, {
+        root: this.scrollableOffsetParent,
+        rootMargin: this.rootMarginValue,
+      })
+      observer.observe(this.nextPageLinkTarget)
+    }
+  }
+
+  loadNextPage() {
+    Rails.ajax({
+      url: this.nextPageLinkTarget.href,
+      type: 'get',
+      success: (data) => {
+        let content = data.getElementById(this.element.id)
+        this.nextPageLinkTarget.outerHTML = content.innerHTML
+        this.observeNextPageLink()
+      }
+    })
+  }
+
+  get scrollableOffsetParent() {
+    let offsetParent = this.element.offsetParent
+    if (offsetParent.scrollHeight > offsetParent.clientHeight) {
+      return offsetParent
+    } else {
+      return null
+    }
+  }
+}
