2b6056a01742f32ee323515b1c3895391f2a286d
Rei <chloerei@gmail.com>
2021-09-21 19:36:13 +0800

    only recommend acount that have followers

--- app/models/account.rb
+++ app/models/account.rb
@@ -35,7 +35,7 @@ class Account < ApplicationRecord
   end
 
   scope :recommend_for, -> (user) {
-    joins(:posts).where("posts.published_at > ?", 3.month.ago).where.not(id: user&.account&.id).order("random()")
+    joins(:posts).where("posts.published_at > ?", 3.month.ago).where("followers_count > 0").where.not(id: user&.account&.id).order("random()")
   }
 
   def to_param
