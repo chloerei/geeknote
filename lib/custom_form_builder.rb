f325f7abc513efa57b5d6412b961ac8d7f6bbffd
Rei <chloerei@gmail.com>
2022-02-10 23:11:16 +0800

    fix user session form style

--- lib/custom_form_builder.rb
+++ lib/custom_form_builder.rb
@@ -2,7 +2,7 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   [:text_field, :password_field, :text_area, :number_field].each do |method|
     define_method(method) do |attribute, options = {}|
       klass = ['text-field', 'text-field--outlined']
-      klass << 'text-field--invalid' if object && object.errors[attribute].any?
+      klass << 'text-field--error' if object && object.errors[attribute].any?
       klass << 'text-field--with-prefix-text' if options[:prefix_text]
       klass << 'text-field--with-suffix-text' if options[:suffix_text]
       @template.content_tag(:div, class: klass) do
@@ -11,7 +11,7 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
             if options[:prefix_text]
               @template.concat @template.content_tag 'div', options[:prefix_text], class: 'text-field__prefix-text'
             end
-            @template.concat super(attribute, options.merge(placeholder: ' '))
+            @template.concat super(attribute, options.merge(placeholder: ' ', class: 'text-field__input'))
             @template.concat label(attribute, options[:label], class: 'text-field__label')
             if options[:suffix_text]
               @template.concat @template.content_tag 'div', options[:suffix_text], class: 'text-field__suffix-text'
@@ -20,7 +20,7 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
         )
 
         if error_text = object && object.errors[attribute].first
-          @template.concat @template.content_tag(:div, error_text, class: 'text-field__error-text')
+          @template.concat @template.content_tag(:div, error_text, class: 'text-field__helper-text')
         elsif options[:helper_text]
           @template.concat @template.content_tag(:div, options[:helper_text], class: 'text-field__helper-text')
         end
