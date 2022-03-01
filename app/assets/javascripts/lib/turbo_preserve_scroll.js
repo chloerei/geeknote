73b25665fc31255b8a42d6e3b66cc0db0f3cd6c6
Rei <chloerei@gmail.com>
2022-03-01 17:06:17 +0800

    remove unused js code

--- app/assets/javascripts/lib/turbo_preserve_scroll.js
+++ app/assets/javascripts/lib/turbo_preserve_scroll.js
@@ -3,13 +3,11 @@ let scrollTop = 0
 window.addEventListener("turbo:click", ({ target }) => {
   if (target.hasAttribute("data-turbo-preserve-scroll")) {
     scrollTop = document.scrollingElement.scrollTop
-    console.log(scrollTop)
   }
 })
 
 window.addEventListener("turbo:render", () => {
   if (scrollTop) {
-    console.log(scrollTop)
     document.scrollingElement.scrollTo(0, scrollTop)
     Turbo.navigator.currentVisit.scrolled = true
   }
--- app/webpacker/packs/application.js
+++
@@ -1,5 +0,0 @@
-import 'javascripts'
-import 'stylesheets'
-
-let imageContext = require.context('images/', true)
-imageContext.keys().forEach(imageContext)
