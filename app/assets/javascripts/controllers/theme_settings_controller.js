5e7432fc255ebb6ec551810c891d5b1bd1607ed6
Rei <chloerei@gmail.com>
2022-03-07 23:48:07 +0800

    fix theme settings init

--- app/assets/javascripts/controllers/theme_settings_controller.js
+++ app/assets/javascripts/controllers/theme_settings_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const colorScheme = localStorage.getItem('color_scheme') || 'default'
+    const colorScheme = localStorage.getItem('color-scheme') || 'default'
     const input = this.element.querySelector(`input[name="color_scheme"][value="${colorScheme}"]`)
     if (input) {
       input.checked = true
