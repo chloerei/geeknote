5f050766937fc8d02bd9d83f301cc3a46bfaa7a1
Rei <chloerei@gmail.com>
2022-04-04 20:14:11 +0800

    reduce feed fetch frequency to 4 hours

--- app/jobs/feed_import_schedule_job.rb
+++ app/jobs/feed_import_schedule_job.rb
@@ -2,7 +2,7 @@ class FeedImportScheduleJob < ApplicationJob
   queue_as :default
 
   def perform
-    Account.where.not(feed_url: nil).find_each do |account|
+    Account.where.not(feed_url: nil).where("feed_fetched_at IS NULL OR feed_fetched_at < ?", 4.hours.ago).find_each do |account|
       FeedImportJob.perform_later(account)
     end
   end
