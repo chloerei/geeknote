b167beed94fa5b856bd5aa760fcf696611a7a267
Rei <chloerei@gmail.com>
2022-04-04 19:45:00 +0800

    trigger feed import when save settings

--- app/controllers/account/dashboard/settings/imports_controller.rb
+++ app/controllers/account/dashboard/settings/imports_controller.rb
@@ -4,6 +4,10 @@ class Account::Dashboard::Settings::ImportsController < Account::Dashboard::Sett
 
   def update
     if @account.update account_params
+      if @account.feed_url.present?
+        FeedImportJob.perform_later(@account)
+      end
+
       redirect_to account_dashboard_settings_import_path(@account), notice: I18n.t('flash.import_settings_updated')
     else
       render turbo_stream: turbo_stream.replace('settings-form', partial: 'form')
