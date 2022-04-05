de2b6fd356915eb5e1ea61c2f4e046cbe16adb01
Rei <chloerei@gmail.com>
2022-04-05 20:13:14 +0800

    add link_to_canonical_url helper for security reason

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -145,4 +145,11 @@ module ApplicationHelper
   def diff_html(from, to)
     sanitize Diffy::Diff.new(from.to_s, to.to_s, allow_empty_diff: false).to_s(:html)
   end
+
+  def link_to_canonical_url(url)
+    uri = URI.parse(url) rescue nil
+    if uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+      link_to uri.host, uri.to_s
+    end
+  end
 end
