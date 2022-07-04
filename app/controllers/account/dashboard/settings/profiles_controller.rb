2cbbc898da99b73ea99d36a773ceb1731c16add9
Rei <chloerei@gmail.com>
2022-07-04 21:47:27 +0800

    update account dashboard settings layout

--- app/controllers/account/dashboard/settings/profiles_controller.rb
+++ app/controllers/account/dashboard/settings/profiles_controller.rb
@@ -6,7 +6,12 @@ class Account::Dashboard::Settings::ProfilesController < Account::Dashboard::Set
   def update
     @owner = @account.owner
 
+    if params[:remove_banner_image]
+      @owner.banner_image = nil
+    end
+
     if @owner.update profile_params
+
       redirect_to account_dashboard_settings_profile_path(@account), notice: I18n.t('flash.profile_updated')
     else
       render turbo_stream: turbo_stream.replace('profile-form', partial: 'form')
@@ -17,9 +22,9 @@ class Account::Dashboard::Settings::ProfilesController < Account::Dashboard::Set
 
   def profile_params
     if @account.organization?
-      params.require(:organization).permit(:name, :description, :display_post_author)
+      params.require(:organization).permit(:name, :description, :display_post_author, :banner_image, :avatar)
     else
-      params.require(:user).permit(:name, :bio)
+      params.require(:user).permit(:name, :bio, :banner_image, :avatar)
     end
   end
 end
