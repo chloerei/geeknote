5f90b50cc3171d125625afcd4531bb7586622f84
Rei <chloerei@gmail.com>
2022-08-04 23:31:32 +0800

    Add sit icon and logo settings

--- app/controllers/admin/settings/appearances_controller.rb
+++ app/controllers/admin/settings/appearances_controller.rb
@@ -3,6 +3,18 @@ class Admin::Settings::AppearancesController < Admin::Settings::BaseController
   end
 
   def update
+    if params[:remove_icon]
+      @site.icon = nil
+    end
+
+    if params[:remove_logo]
+      @site.logo = nil
+    end
+
+    if params[:remove_logo_alt]
+      @site.logo_alt = nil
+    end
+
     if @site.update site_params
       redirect_to admin_settings_appearance_path, notice: I18n.t('flash.appearance_update_successful')
     else
@@ -13,6 +25,6 @@ class Admin::Settings::AppearancesController < Admin::Settings::BaseController
   private
 
   def site_params
-    params.require(:site).permit(:name, :description)
+    params.require(:site).permit(:name, :description, :icon, :logo, :logo_alt)
   end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,13 @@
 class Site < ApplicationRecord
+  has_one_attached :icon do |attachable|
+    attachable.variant :normal, resize_to_fit: [256, 256]
+  end
+
+  has_one_attached :logo do |attachable|
+    attachable.variant :normal, resize_to_limit: [256, 256]
+  end
+
+  has_one_attached :logo_alt do |attachable|
+    attachable.variant :normal, resize_to_limit: [256, 256]
+  end
 end
