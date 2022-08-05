eb7fd6bc627d657b57fc165746c818904ee6d8e3
Rei <chloerei@gmail.com>
2022-08-05 17:11:31 +0800

    Add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ end
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  get 'contact', to: 'pages#contact'
-
   get 'sign_up', to: 'users#new', as: 'sign_up'
   resources :users, only: [:create]
   get 'sign_in', to: 'sessions#new', as: 'sign_in'
