5f27edc35d30f8c3101eb2210bec7672346bbd39
Rei <chloerei@gmail.com>
2022-08-06 20:20:29 +0800

    use original logo size

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,13 +3,9 @@ class Site < ApplicationRecord
     attachable.variant :normal, resize_to_fit: [256, 256]
   end
 
-  has_one_attached :logo do |attachable|
-    attachable.variant :normal, resize_to_limit: [256, 256]
-  end
+  has_one_attached :logo
 
-  has_one_attached :logo_alt do |attachable|
-    attachable.variant :normal, resize_to_limit: [256, 256]
-  end
+  has_one_attached :logo_alt
 
   def remove_icon=(value)
     if value
