2b18ceda3ead7ff3dc197cc3ba5a99a233a155c2
Rei <chloerei@gmail.com>
2022-08-08 21:54:58 +0800

    add recaptcha in sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,23 @@ class SessionsController < ApplicationController
       uri = URI(params[:return_to])
       session[:return_to] = [uri.path, uri.query].compact.join('?')
     end
+
+    @user = User.new
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    @user = User.find_or_initialize_by(email: params[:user][:email])
 
-    if user&.authenticate(params[:password])
-      sign_in(user)
-      redirect_to session.delete(:return_to) || root_path
+    if optional_verify_recaptcha(model: @user)
+      if @user.authenticate(params[:user][:password])
+        sign_in(@user)
+        redirect_to session.delete(:return_to) || root_path
+      else
+        @user.errors.add(:base, :email_or_password_error)
+        render :new, status: :unprocessable_entity
+      end
     else
-      @sign_in_error = true
-      render turbo_stream: turbo_stream.replace('sign-in-form', partial: 'form')
+      render :new, status: :unprocessable_entity
     end
   end
 
