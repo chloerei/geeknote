9f80823815f5dc40cf13d976baf9ac63a4ca4699
Rei <chloerei@gmail.com>
2022-10-22 22:00:22 +0800

    edit tags page layout

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,14 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.trending.limit(25)
+  end
+
   def show
     @tag = Tag.find_by! name: params[:id]
     @posts = Post.published.tagged_with(params[:id]).page(params[:page])
 
     respond_to do |format|
-      format.html { render 'home/index' }
+      format.html
     end
   end
 end
