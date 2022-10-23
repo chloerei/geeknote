dbf74cf289f530c1e4cd4b033704497af271b034
Rei <chloerei@gmail.com>
2022-10-23 21:46:22 +0800

    fix tag posts order

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find_by! name: params[:id]
-    @posts = Post.published.tagged_with(params[:id]).hot.where("published_at > ?", 3.month.ago).page(params[:page])
+    @posts = Post.published.tagged_with(params[:id]).order(published_at: :desc).page(params[:page])
 
     respond_to do |format|
       format.html
