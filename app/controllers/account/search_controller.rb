598c5813eb445941c33334e8cca7e6df253ad766
Rei <chloerei@gmail.com>
2023-03-30 19:42:58 +0800

    add default search result

--- app/controllers/account/search_controller.rb
+++ app/controllers/account/search_controller.rb
@@ -2,6 +2,8 @@ class Account::SearchController < Account::BaseController
   def index
     if params[:query].present?
       @posts = Post.search(params[:query], filter: "status = 'published' AND account_id = #{@account.id}").page(params[:page])
+    else
+      @posts = Post.published.where(account_id: @account.id).order(published_at: :desc).page(params[:page])
     end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   def index
     if params[:query].present?
       @posts = Post.search(params[:query], filter: "status = 'published'").page(params[:page])
+    else
+      @posts = Post.published.order(score: :desc).page(params[:page])
     end
   end
 end
