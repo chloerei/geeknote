be3739ae87feabc335cd8388569f83502e03c01f
Rei <chloerei@gmail.com>
2023-03-30 20:27:58 +0800

    preload post cache key

--- app/controllers/account/search_controller.rb
+++ app/controllers/account/search_controller.rb
@@ -1,9 +1,9 @@
 class Account::SearchController < Account::BaseController
   def index
     if params[:query].present?
-      @posts = Post.search(params[:query], filter: "status = 'published' AND account_id = #{@account.id}").page(params[:page])
+      @posts = Post.includes(:account, :user).search(params[:query], filter: "status = 'published' AND account_id = #{@account.id}").page(params[:page])
     else
-      @posts = Post.published.where(account_id: @account.id).order(published_at: :desc).page(params[:page])
+      @posts = Post.published.includes(:account, :user).where(account_id: @account.id).order(published_at: :desc).page(params[:page])
     end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
     if params[:query].present?
-      @posts = Post.search(params[:query], filter: "status = 'published'").page(params[:page])
+      @posts = Post.includes(:account, :user).search(params[:query], filter: "status = 'published'").page(params[:page])
     else
-      @posts = Post.published.order(score: :desc).page(params[:page])
+      @posts = Post.published.includes(:account, :user).order(score: :desc).page(params[:page])
     end
   end
 end
