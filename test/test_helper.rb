08cadb0ab94e3f64e48b0c59cf3fee83e46a8651
Rei <chloerei@gmail.com>
2023-04-04 22:48:24 +0800

    fix meilisearch test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'webmock/minitest'
 
+WebMock.disable_net_connect!(allow: [
+  "meilisearch:7700"
+])
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
