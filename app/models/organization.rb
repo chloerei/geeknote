3c4ddad4e4358253038c05ae6943a1878a2a8e6b
Rei <chloerei@gmail.com>
2024-06-12 19:40:39 +0800

    fix org avatar

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,14 @@
 class Organization < ApplicationRecord
   has_one :account, as: :owner, autosave: true
   has_many :members
-  has_one_attached :avatar
-  has_one_attached :banner_image
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_fill: [ 320, 320 ], preprocessed: true
+  end
+
+  has_one_attached :banner_image do |attachable|
+    attachable.variant :large, resize_to_limit: [ 1920, 1920 ], preprocessed: true
+  end
 
   validates :name, presence: true
 
