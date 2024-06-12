2e79b396d050308ac6e9d7894b77bb6ba4619902
Rei <chloerei@gmail.com>
2024-06-12 19:27:08 +0800

    fix logo storage url

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,6 @@
 class Site < ApplicationRecord
   has_one_attached :icon do |attachable|
-    attachable.variant :normal, resize_to_fit: [ 256, 256 ]
+    attachable.variant :normal, resize_to_fit: [ 512, 512 ], preprocessed: true
   end
 
   has_one_attached :logo
