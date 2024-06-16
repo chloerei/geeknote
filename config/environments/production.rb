d44b7691993bad5c5a34e912d9310d0623b8e72f
Rei <chloerei@gmail.com>
2024-06-17 03:02:32 +0800

    Add assume_ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = ENV["FORCE_SSL"].present?
 
+  config.assume_ssl = true
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = ENV.fetch("LOG_LEVEL", :info)
