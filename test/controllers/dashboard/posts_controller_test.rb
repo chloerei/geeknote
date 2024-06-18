b086085beaaa70967367e9535c8b0e949fcf9e08
Rei <chloerei@gmail.com>
2024-06-18 21:26:02 +0800

    test post scoe in post edit action, avoid search index not complete in test

--- test/controllers/dashboard/posts_controller_test.rb
+++ test/controllers/dashboard/posts_controller_test.rb
@@ -9,7 +9,6 @@ class Dashboard::PostsControllerTest < ActionDispatch::IntegrationTest
 
     get dashboard_posts_url(user.account.name)
     assert_response :success
-    assert_select ".post-item", 1
   end
 
   test "should get index in org as admin" do
@@ -23,7 +22,6 @@ class Dashboard::PostsControllerTest < ActionDispatch::IntegrationTest
 
     get dashboard_posts_url(organization.account.name)
     assert_response :success
-    assert_select ".post-item", 2
   end
 
   test "should get index in org as member" do
@@ -37,7 +35,6 @@ class Dashboard::PostsControllerTest < ActionDispatch::IntegrationTest
 
     get dashboard_posts_url(organization.account.name)
     assert_response :success
-    assert_select ".post-item", 1
   end
 
   test "should get new" do
@@ -59,13 +56,57 @@ class Dashboard::PostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to edit_dashboard_post_url(user.account.name, Post.last)
   end
 
-  test "should get edit" do
+  test "should get edit as personal" do
     user = create(:user)
     post = create(:post, user: user)
     sign_in user
 
     get edit_dashboard_post_url(user.account.name, post)
     assert_response :success
+  end
+
+  test "should not get other edit as personal" do
+    user = create(:user)
+    post = create(:post)
+    sign_in user
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get edit_dashboard_post_url(user.account.name, post)
+    end
+  end
+
+  test "should get edit as admin" do
+    user = create(:user)
+    organization = create(:organization)
+    create(:member, user: user, organization: organization, role: "admin")
+    post = create(:post, account: organization.account)
+    sign_in user
+
+    get edit_dashboard_post_url(organization.account.name, post)
+    assert_response :success
+  end
+
+  test "should get edit as member" do
+    user = create(:user)
+    organization = create(:organization)
+    create(:member, user: user, organization: organization, role: "member")
+    post = create(:post, account: organization.account, user: user)
+    sign_in user
+
+    get edit_dashboard_post_url(organization.account.name, post)
+    assert_response :success
+  end
+
+  test "should not get other edit as member" do
+    user = create(:user)
+    organization = create(:organization)
+    create(:member, user: user, organization: organization, role: "member")
+    post = create(:post, account: organization.account)
+    sign_in user
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get edit_dashboard_post_url(organization.account.name, post)
+    end
   end
 
   test "should update post" do
