0960f94ac47a4c0932faa560ca898e64a0ef29a2
Rei <chloerei@gmail.com>
2024-07-08 20:03:38 +0800

    fix home following query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,21 @@ class HomeController < ApplicationController
   before_action :require_sign_in, only: [ :following ]
 
   def index
-    @pagy, @posts = pagy(Post.published.order(score: :desc), page: params[:page])
+    @pagy, @posts = pagy(Post.published.order(score: :desc))
   end
 
   def following
-    # Todo
-    @pagy, @posts = pagy(Post.published.order(published_at: :desc), page: params[:page])
+    follow_accounts = Current.user.followings
+    follow_users = Current.user.following_users
+    @pagy, @posts = pagy(
+      Post.published.order(published_at: :desc).where(account: follow_accounts)
+        .or(Post.published.order(published_at: :desc).where(user: follow_users))
+    )
     render :index
   end
 
   def newest
-    @pagy, @posts = pagy(Post.published.order(published_at: :desc), page: params[:page])
+    @pagy, @posts = pagy(Post.published.order(published_at: :desc))
     render :index
   end
 end
