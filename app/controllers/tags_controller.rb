070d11fd5a9c6e085c16702273dbdc6a87cd3a4a
Rei <chloerei@gmail.com>
2025-01-24 20:14:17 +0800

    fix home sidebar

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,6 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find_by! name: params[:id]
-    @posts = Post.published.tagged_with(params[:id]).order(published_at: :desc).page(params[:page])
-    @paginator = RailsCursorPagination::Paginator.new(Post.published.tagged_with(params[:id]), order_by: :published_at, order: :desc, after: params[:after]).fetch
+    @pagy, @posts = pagy(Post.published.tagged_with(params[:id]).order(published_at: :desc))
   end
 end
