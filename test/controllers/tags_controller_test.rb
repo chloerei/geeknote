faf2e4dd52cbba87d6db234bb1b20ae6293ed5be
Rei <chloerei@gmail.com>
2025-04-12 21:36:14 +0800

    fix home sidebar nav target

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class TagsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get show" do
+    tag = Tag.create(name: "Foobar")
+    get tag_url(tag.name)
+    assert_response :success
+  end
 end
