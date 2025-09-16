36b7efd5acf9a3d08befed75cfa334796d9fd5c4
Rei <chloerei@gmail.com>
2025-09-16 14:38:57 +0800

    add hotkey global attribute

--- app/javascript/controllers/hotkey_controller.js
+++ app/javascript/controllers/hotkey_controller.js
@@ -1,17 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
-import { install, uninstall } from "@github/hotkey"
+import { install, uninstall, eventToHotkeyString, normalizeHotkey } from "@github/hotkey"
+import { isFormField } from "@github/hotkey/dist/utils.js"
 
 // Connects to data-controller="hotkey"
 export default class extends Controller {
-  static values = {
-    global: { type: Boolean, default: false }
-  }
-
   connect() {
-    install(this.element)
+    if (this.element.hasAttribute("data-hotkey-global")) {
+      this.keydownHandler = (event) => {
+        const hotkey = eventToHotkeyString(event)
+        const targetHotkey = normalizeHotkey(this.element.getAttribute("data-hotkey"))
+        if (hotkey === targetHotkey) {
+          event.preventDefault()
+          if (isFormField(this.element)) {
+            this.element.focus()
+          } else {
+            this.element.click()
+          }
+        }
+      }
+      document.addEventListener("keydown", this.keydownHandler)
+    } else {
+      install(this.element)
+    }
   }
 
   disconnect() {
-    uninstall(this.element)
+    if (this.keydownHandler) {
+      document.removeEventListener("keydown", this.keydownHandler)
+    } else {
+      uninstall(this.element)
+    }
   }
 }
