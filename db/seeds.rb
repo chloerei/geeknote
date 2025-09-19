c6462532f7920c5b58f9cc027b7688fdeb82925b
Rei <chloerei@gmail.com>
2025-09-19 16:34:58 +0800

    add dev seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  user = User.create!(
+    name: "Dev",
+    email: "dev@example.com",
+    password: "password",
+    account_attributes: {
+      name: "dev"
+    }
+  )
+
+  post = Post.create!(
+    account: user.account,
+    user: user,
+    status: "published",
+    published_at: Time.now,
+    title: "README",
+    content: File.read(Rails.root.join("README.md")),
+    tag_list: "Ruby, JavaScript",
+  )
+
+  Comment.create!(
+    commentable: post,
+    user: user,
+    content: "This is a comment on the README post."
+  )
+end
