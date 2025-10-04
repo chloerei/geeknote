690cae1b54a0abbcd966cf186afa19321a311f6e
Rei <chloerei@gmail.com>
2025-10-04 19:27:38 +0800

    edit analytics charts

--- app/controllers/dashboard/analytics/home_controller.rb
+++ app/controllers/dashboard/analytics/home_controller.rb
@@ -1,6 +1,16 @@
 class Dashboard::Analytics::HomeController < Dashboard::Analytics::BaseController
   def index
-    @post_views_data = Ahoy::Event.where_event("post_view", account_id: @account.id).group_by_day(:time, last: 7).count
-    @references_data = Ahoy::Visit.where(id: Ahoy::Event.where_event("post_view", account_id: @account.id).select(:visit_id)).group(:referrer).order("count_all DESC").limit(5).count
+    day = 7
+
+    @post_views_data = Ahoy::Event.where_event("post_view", account_id: @account.id).group_by_day(:time, last: day).count
+
+    @top_posts = @account.posts
+      .joins("INNER JOIN ahoy_events ON ahoy_events.properties->>'post_id' = CAST(posts.id AS TEXT)")
+      .where("ahoy_events.name = ?", "post_view")
+      .where("ahoy_events.time >= ?", day.days.ago.beginning_of_day)
+      .group("posts.id")
+      .select("posts.*, COUNT(ahoy_events.id) AS range_views_count")
+      .order("range_views_count DESC")
+      .limit(5)
   end
 end
