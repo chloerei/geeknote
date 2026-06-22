ece48aa278d06a369b68209d6fa39e7aef14316d
Rei <chloerei@gmail.com>
2026-06-22 08:26:46 +0000

    Make scroll pagination auto-load configurable

--- app/javascript/controllers/scroll_pagination_controller.js
+++ app/javascript/controllers/scroll_pagination_controller.js
@@ -1,39 +1,64 @@
-import { Controller } from "@hotwired/stimulus"
-import { get } from '@rails/request.js'
+import { Controller } from "@hotwired/stimulus";
+import { get } from "@rails/request.js";
 
 // Connects to data-controller="scroll-pagination"
 export default class extends Controller {
-  static targets = [ "content", "next", "nextLink" ]
+  static targets = ["content", "next", "nextLink"];
+
+  static values = {
+    autoLoad: { type: Boolean, default: false },
+  };
 
   connect() {
-    this.observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        this.observer.unobserve(this.nextLinkTarget)
-        this.loadNextPage()
-      }
-    }, {
-      rootMargin: '1000px',
-    })
-
-    if (this.hasNextLinkTarget) {
-      this.observer.observe(this.nextLinkTarget)
+    if (this.autoLoadValue) {
+      this.createObserver();
     }
   }
 
   disconnect() {
-    this.observer.disconnect()
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  createObserver() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          this.observer.unobserve(this.nextLinkTarget);
+          this.loadNextPage();
+        }
+      },
+      {
+        rootMargin: "1000px",
+      },
+    );
+
+    if (this.hasNextLinkTarget) {
+      this.observer.observe(this.nextLinkTarget);
+    }
   }
 
   async loadNextPage() {
-    const response = await get(this.nextLinkTarget.href)
+    const response = await get(this.nextLinkTarget.href);
     if (response.ok) {
-      const html = await response.html
-      const dom = new DOMParser().parseFromString(html, 'text/html')
-      this.contentTarget.insertAdjacentHTML('beforeend', dom.getElementById(this.contentTarget.id).innerHTML)
-      this.nextTarget.innerHTML = dom.getElementById(this.nextTarget.id).innerHTML
-      if (this.hasNextLinkTarget) {
-        this.observer.observe(this.nextLinkTarget)
+      const html = await response.html;
+      const dom = new DOMParser().parseFromString(html, "text/html");
+      this.contentTarget.insertAdjacentHTML(
+        "beforeend",
+        dom.getElementById(this.contentTarget.id).innerHTML,
+      );
+      this.nextTarget.innerHTML = dom.getElementById(
+        this.nextTarget.id,
+      ).innerHTML;
+      if (this.observer && this.hasNextLinkTarget) {
+        this.observer.observe(this.nextLinkTarget);
       }
     }
+  }
+
+  load(event) {
+    event.preventDefault();
+    this.loadNextPage();
   }
 }
