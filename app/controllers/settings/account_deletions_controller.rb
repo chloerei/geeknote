e8e3a567a7c085dda94a56fe907cbfd54e031bcf
Rei <chloerei@gmail.com>
2026-08-06 02:38:05 +0000

    Block account deletion for users in organizations

--- app/controllers/settings/account_deletions_controller.rb
+++ app/controllers/settings/account_deletions_controller.rb
@@ -1,10 +1,15 @@
 class Settings::AccountDeletionsController < Settings::BaseController
+  before_action :set_organizations
+
   def show
     @page_titles.prepend t("general.delete_account")
   end
 
   def create
-    if params[:name] == @user.account.name
+    if @organizations.any?
+      flash.now[:alert] = t(".must_leave_organizations")
+      render :show, status: :unprocessable_content
+    elsif params[:name] == @user.account.name
       UserDeletionJob.perform_later(@user)
       terminate_session
       redirect_to account_deleted_path
@@ -12,5 +17,11 @@ class Settings::AccountDeletionsController < Settings::BaseController
       flash.now[:alert] = t(".account_name_mismatch")
       render :show, status: :unprocessable_content
     end
+  end
+
+  private
+
+  def set_organizations
+    @organizations = Current.user.organizations
   end
 end
--- test/controllers/settings/account_deletions_controller_test.rb
+++ test/controllers/settings/account_deletions_controller_test.rb
@@ -37,6 +37,31 @@ class Settings::AccountDeletionsControllerTest < ActionDispatch::IntegrationTest
     assert User.exists?(@user.id)
   end
 
+  test "should not show deletion form when user belongs to an organization" do
+    organization = create(:organization, name: "Test Org")
+    create(:member, organization: organization, user: @user)
+    sign_in @user
+
+    get settings_account_deletion_path
+    assert_response :success
+    assert_select "form[action=?]", settings_account_deletion_path, count: 0
+  end
+
+  test "should not schedule deletion when user belongs to an organization" do
+    organization = create(:organization)
+    create(:member, organization: organization, user: @user)
+    sign_in @user
+    session = @user.sessions.last
+
+    assert_no_enqueued_jobs only: UserDeletionJob do
+      post settings_account_deletion_path, params: { name: @user.account.name }
+    end
+
+    assert_response :unprocessable_content
+    assert Session.exists?(session.id)
+    assert User.exists?(@user.id)
+  end
+
   test "should require authentication" do
     get settings_account_deletion_path
     assert_redirected_to new_session_url
